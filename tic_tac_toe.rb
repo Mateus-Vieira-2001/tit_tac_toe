94adaaa6ca6740a75848be88e96a02b5e1046b90
Mateus Vieira <mateus.vieira@sidedoor.com.br>
2024-01-31 09:53:41 -0300

    desenvolvimento-31-01-2024

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,40 +1,59 @@
-#jogo da velha
+# frozen_string_literal: true
+
+# jogo da velha
 module TicTacToe
   puts '___|___|___'
   puts '___|___|___'
   puts '   |   |   '
+
   class Player
-      def initialize(name,character)
-        @name=name
-        @character=character
-      end
-
-      def to_s
-        "#{@name} (#{@character})"
-      end
-  end
-
-
-
-  class Game
-
-    def self.Board(columns,row,board)
-
-      board[columns.to_i][row.to_i]
+		#attr_reader :name,:character
+    def initialize(name, character)
+      @name = name
+      @character = character
     end
-    board = [[0,0,0],[0,0,0],[0,0,0]]
-
-    turn = 0
-    case turn
-    when 0
-      puts "Jodador 1, selecione uma posição no tabuleiro"
-      columns = gets.chomp
-      row = gets.chomp
-      self.Board(columns,row,board)
-    end
+		def dados_completos
+			@masculino='masculino'
+		end
+		def name
+			@name
+		end
+		def character
+			@character
+		end
+		def sexo
+			@masculino
+		end
   end
+end
+#mateus=TicTacToe::Player.new('Mateus','x')
+  #class Game
+  #   def initialize(player1, player2)
+  #     @players = [player1, player2]
+	# 		@board = [[nil,nil,nil],[nil,nil,nil],[nil,nil,nil]]
+  #   end
 
-  player1 = Player.new("player1","X")
-  player2 = Player.new("player2","O")
+  #   def tabuleiro(columns, row, turn)
+  #     @board[columns.to_i][row.to_i] = @players[turn.to_i]
+  #   end
 
+  #   turn = 0
+  #   while turn < 9
+  #     if turn.even?
+  #       puts 'Jodador 1, selecione uma posição no tabuleiro'
+  #     else
+  #       puts 'Jodador 2, selecione uma posição no tabuleiro'
+  #     end
+  #     columns = gets.chomp
+  #     row = gets.chomp
+
+  #     #tabuleiro(columns, row, turn)
+  #     turn += 1
+  #   end
+  # end
+
+
+
+  #Player.new('player1', 'X')
+  #Player.new('player2', 'O')
 end
